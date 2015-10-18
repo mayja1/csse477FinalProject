7b687fcabec38bb80cf25f944d7b5e2a6cbbbce6
taylorj7 <taylorj7@rose-hulman.edu>
2015-10-18 14:49:24 -0400

    Request Handlers

--- edu.rosehulman.sws/src/protocol/RequestHandler.java
+++ edu.rosehulman.sws/src/protocol/RequestHandler.java
@@ -28,10 +28,33 @@
  
 package protocol;
 
+import java.io.File;
+
+import server.Server;
+
 /**
  * 
  * @author Chandan R. Rupakheti (rupakhcr@clarkson.edu)
  */
-public interface RequestHandler {
-	HttpResponse Process(HttpRequest h);
+public abstract class RequestHandler {
+	public HttpResponse Process(HttpRequest h, Server s){
+		HttpResponse response = null;
+		// Handling GET request here
+		// Get relative URI path from request
+		String uri = h.getUri();
+		// Get root directory path from server
+		String rootDirectory = s.getRootDirectory();
+		// Combine them together to form absolute file path
+		File file = new File(rootDirectory + uri);
+		// Check if the file exists
+		if(file.exists()) {
+			response = file_exists(rootDirectory, uri, file, h);
+		}
+		else {
+			response = file_no_exist(rootDirectory, uri, file, h);
+		}
+		return response;
+	}
+	abstract HttpResponse file_exists(String rootDirectory,String uri, File f, HttpRequest hr);
+	abstract HttpResponse file_no_exist(String rootDirectory,String uri, File f, HttpRequest hr);
 }
